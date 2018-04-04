64df7d188e253c8c3355fe5da22f939624fc74fb
timur27 <timur.karimow.95@gmail.com>
2018-04-04 12:06:52 +0200

    Delete unused annotations

--- 8 - Generator-Transakcji-DI/src/main/java/Reader/Parser.java
+++ 8 - Generator-Transakcji-DI/src/main/java/Reader/Parser.java
@@ -6,7 +6,6 @@ import Model.Transaction;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -22,7 +21,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 
 @Service
-@Lazy
 public class Parser {
 
     private static final Logger log = LoggerFactory.getLogger("sda");
