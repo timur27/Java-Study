afa7115abb4e3e6c7564d7c0cca2e68212a458c5
timur27 <timur.karimow.95@gmail.com>
2018-05-19 14:42:13 +0200

    Changed yaml xml filename

--- 8 - Generator-Transakcji-DI/src/main/java/Generator/Generator.java
+++ 8 - Generator-Transakcji-DI/src/main/java/Generator/Generator.java
@@ -35,8 +35,6 @@ public class Generator {
             return;
 
         int eventCount = Integer.valueOf(resultTransaction.getEventsCount());
-        File file;
-        file.createNewFile();
 
         SavedObjectList savedObjectList = new SavedObjectList();
         for (int i = 0; i < eventCount; i++){
@@ -45,19 +43,22 @@ public class Generator {
             savedObjectList.add(objectToWrite);
         }
         if (resultTransaction.getFormatOption().equals("xml")){
-            file = new File(resultTransaction.getOutDir() + ".xml");
+            File file = new File(resultTransaction.getOutDir() + ".xml");
+            file.createNewFile();
             log.info("Begin with mapping to xml");
             xmlWriter.writeToFile(savedObjectList,file);
             log.info("Successfully done!");
         }
         else if(resultTransaction.getFormatOption().equals("json")){
-            file = new File(resultTransaction.getOutDir() + ".json");
+            File file = new File(resultTransaction.getOutDir() + ".json");
+            file.createNewFile();
             log.info("Begin with mapping to json");
             jsonWriter.writeToFile(savedObjectList.getSavedObjects(), file);
             log.info("Successfully done!");
         }
         else if (resultTransaction.getFormatOption().equals("yaml")){
-            file = new File(resultTransaction.getOutDir() + ".yaml");
+            File file = new File(resultTransaction.getOutDir() + ".yaml");
+            file.createNewFile();
             log.info("Begin with mapping to YAML");
             yamlWriter.writeToFile(savedObjectList.getSavedObjects(), file);
             log.info("Successfully done!");
